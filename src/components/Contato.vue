7c158c78c1111b431364c047ccaaaae9d8c50981
Katsu <xjverudonx@gmail.com>
2023-05-09 17:52:38 -0300

    v1

---
+++ src/components/Contato.vue
@@ -0,0 +1,47 @@
+<template>
+<footer class="bg-kldarkblue pt-10 mt-10" id="Contato">
+
+    <div class="grid grid-cols-1 lg:grid-cols-4 justify-items-center gap-4 ">
+        
+        
+        <div>
+        <a type="button" href="#" class="">
+        <img class="w-16 duration-300 group-hover:scale-110 mx-auto" src="\assets\img\linkedin.png" alt="" />
+        <p class="text-white text-xl font-medium text-center pt-2">Linkedin</p>
+        <span class="text-center text-grey-400">@joaovictorsdomingues</span>
+        </a>
+        </div>
+
+        <div>
+        <a type="button" href="#" class="">
+        <img class="w-16 duration-300 group-hover:scale-110 mx-auto" src="\assets\img\github.png" alt="" />
+        <p class="text-white text-xl font-medium text-center pt-2">GitHub</p>
+        <span class="text-center text-grey-400">@TheKatsu1</span>
+        </a>
+        </div>
+
+        <div>
+        <a type="button" href="#" class="">
+        <img class="w-16 duration-300 group-hover:scale-110 mx-auto" src="\assets\img\whatsapp.png" alt="" />
+        <p class="text-white text-xl font-medium text-center pt-2">Whatsapp</p>
+        <span class="text-center text-grey-400">(24) 99944-5848</span>
+        </a>
+        </div>
+
+        <div>
+        <a type="button" href="#" class="">
+        <img class="w-16 duration-300 group-hover:scale-110 mx-auto" src="\assets\img\email.png" alt="" />
+        <p class="text-white text-xl font-medium text-center pt-2">E-mail</p>
+        <span class="text-center text-grey-400">thekatsu2@gmail.com</span>
+        </a>
+        </div>
+
+        </div>
+
+        <div class="flex flex-col pt-10">
+            <h3 class="text-center text-white">Todos os Direitos Reservados © </h3>
+        </div>
+
+   
+</footer>
+</template>
---
+++ src/components/Experiencia.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="flex-1 my-20 pt-32" id="Experiencia">
+        <h2 class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">Experiência</h2>
+    </div>
+<ol class="items-center sm:flex px-14">
+    <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+            <div class="z-10 flex items-center justify-center w-6 h-6 bg-kldarkblue rounded-full ring-0 ring-white sm:ring-8 dark:ring-gray-900 shrink-0">
+                <i class="fa-solid fa-graduation-cap" style="color: #506ca4; font-size: small;"></i>
+            </div>
+            <div class="hidden sm:flex w-full bg-klblue h-0.5"></div>
+        </div>
+        <div class="mt-3 sm:pr-8">
+            <h3 class="text-lg font-bold text-kldarkblue">Técnico de Informática</h3>
+            
+            <time class="block mb-2 text-sm font-normal leading-none text-klblue"><i class="fa-regular fa-calendar text-klblue"></i> 2018 - 2020</time>
+            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Entre 2018 e 2020 completei o curso técnico de informática na Escola Técnica Pandiá Calógeras (ETPC), onde tive minha base sobre o mercado de TI.</p>
+        </div>
+    </li>
+    <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+            <div class="z-10 flex items-center justify-center w-6 h-6 bg-kldarkblue rounded-full ring-0 ring-white sm:ring-8 dark:ring-gray-900 shrink-0">
+                <i class="fa-solid fa-book-open" style="color: #506ca4; font-size: small;"></i>
+            </div>
+            <div class="hidden sm:flex w-full bg-klblue h-0.5"></div>
+        </div>
+        <div class="mt-3 sm:pr-8">
+            <h3 class="text-lg font-bold text-kldarkblue">Projetos Pessoais</h3>
+            
+            <time class="block mb-2 text-sm font-normal leading-none text-klblue"><i class="fa-regular fa-calendar text-klblue"></i> 2018 - Presente</time>
+            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Juntamente com meus estudos, busco desenvolver projetos pessoais para melhorar minhas habilidades e estar sempre atualizado.</p>
+        </div>
+    </li>
+    <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+            <div class="z-10 flex items-center justify-center w-6 h-6 bg-kldarkblue rounded-full ring-0 ring-white sm:ring-8 dark:ring-gray-900 shrink-0">
+                <i class="fa-solid fa-graduation-cap" style="color: #506ca4; font-size: small;"></i>
+            </div>
+            <div class="hidden sm:flex w-full bg-klblue h-0.5"></div>
+        </div>
+        <div class="mt-3 sm:pr-8">
+            <h3 class="text-lg font-bold text-kldarkblue">Sistemas de Informação</h3>
+            
+            <time class="block mb-2 text-sm font-normal leading-none text-klblue"><i class="fa-regular fa-calendar text-klblue"></i> 2021 - 2024</time>
+            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Em 2021 inicei minha graduação em Sistemas de Informação na UniFOA, com previsão de conclusão ao final de 2024</p>
+        </div>
+    </li>
+
+    <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+            <div class="z-10 flex items-center justify-center w-6 h-6 bg-kldarkblue rounded-full ring-0 ring-white sm:ring-8 dark:ring-gray-900 shrink-0">
+                <i class="fa-solid fa-briefcase" style="color: #506ca4; font-size: small;"></i>
+            </div>
+            <div class="hidden sm:flex w-full bg-klblue h-0.5"></div>
+        </div>
+        <div class="mt-3 sm:pr-8">
+            <h3 class="text-lg font-bold text-kldarkblue">Desenvolvedor Front-End</h3>
+            
+            <time class="block mb-2 text-sm font-normal leading-none text-klblue"><i class="fa-regular fa-calendar text-klblue"></i> 2022 - 2023</time>
+            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Estágio na empresa DEC9 - Marketing e Tecnologia onde atuei como desenvolver front-end sendo responsável pela criação de websites e landing pages.</p>
+        </div>
+    </li>
+</ol>
+
+
+
+
+
+
+
+
+
+
+</template>
