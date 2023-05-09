6244d6de4dfb7415caa57ae2a27f6ab1e1ef5a80
Katsu <xjverudonx@gmail.com>
2023-05-09 18:36:43 -0300

    Alteração Links

--- src/components/Habilidades.vue
+++ src/components/Habilidades.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="bg-klwhite pt-32" id="Habilidades">
     <div class="flex-1">
-        <h2 class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">Habilidades</h2>
+      <h2
+        class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">
+        Habilidades</h2>
     </div>
 
 
-<!-- <div class="flex flex-row mt-24 ">
+    <!-- <div class="flex flex-row mt-24 ">
 
 <div clas="">
     <img src="\assets\img\html.png" class="w-3/12 m-auto" alt="Killua">
@@ -47,49 +49,49 @@
 </div> -->
 
 
-<div class="grid grid-cols-2 gap-4 p-8 lg:grid-cols-4">
+    <div class="grid grid-cols-2 gap-4 p-8 lg:grid-cols-4">
 
-    <div class="rounded-xl card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white">
-      <figure class="p-8">
-        <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\html.png" alt="" />
-      </figure>
-    </div>
       <div class="rounded-xl card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\css.png" alt="" />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\html.png" alt="" />
+        </figure>
+      </div>
+      <div class="rounded-xl card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white">
+        <figure class="p-8">
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\css.png" alt="" />
         </figure>
       </div>
       <div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white ">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\js.png" alt="" />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\js.png" alt="" />
         </figure>
       </div>
       <div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white ">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\vue.png" alt=""  />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\vue.png" alt="" />
         </figure>
       </div>
       <div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white ">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\bootstrap.png" alt="" />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\bootstrap.png" alt="" />
         </figure>
       </div>
       <div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white ">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\tailwind.png" alt=""  />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\tailwind.png" alt="" />
         </figure>
       </div>
       <div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white ">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\git.png" alt=""  />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\git.png" alt="" />
         </figure>
       </div>
       <div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white ">
         <figure class="p-8">
-          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="\assets\img\php.png" alt="" />
+          <img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\img\php.png" alt="" />
         </figure>
       </div>
-   </div>
+    </div>
 
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,26 @@
 <template>
-    
-<div class="grid grid-cols-1 lg:grid-cols-2 mt-20">
-    <div class="text-center">
-    <h1 class="text-8xl mb-5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">Katsu</h1>
-    <h2 class="text-4xl mb-5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">Front-End Developer</h2>
-    <p class="mx-16 mb-5 text-black text-justify">Meu nome é João Victor Silveira Domingues mas todo mundo me chama pelo meu apelido (Katsu), estudo assuntos relacionados a tecnologia desde 2018 quando comecei meu curso técnico em informática e posso dizer que sou apaixonado pelo desenvolvimento front-end e pelos conceitos de UX/UI Design.</p>
+    <div class="grid grid-cols-1 lg:grid-cols-2 mt-20">
+        <div class="text-center">
+            <h1
+                class="text-8xl mb-5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">
+                Katsu</h1>
+            <h2
+                class="text-4xl mb-5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center">
+                Front-End Developer</h2>
+            <p class="mx-16 mb-5 text-black text-justify">Meu nome é João Victor Silveira Domingues mas todo mundo me chama
+                pelo meu apelido (Katsu), estudo assuntos relacionados a tecnologia desde 2018 quando comecei meu curso
+                técnico em informática e posso dizer que sou apaixonado pelo desenvolvimento front-end e pelos conceitos de
+                UX/UI Design.</p>
 
-    <a type="button" href="#" class="text-white bg-gradient-to-br from-kldarkblue to-klblue hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 font-medium rounded-lg text-xl px-8 py-4 text-center mb-8">Contato</a>
+            <a type="button" href="#"
+                class="text-white bg-gradient-to-br from-kldarkblue to-klblue hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 font-medium rounded-lg text-xl px-8 py-4 text-center mb-8">Contato</a>
+        </div>
+
+
+
+        <div>
+            <img src="assets\img\killua_header2.png" class="max-w-sm ml-10 lg:ml-20" alt="Killua">
+        </div>
+
     </div>
-
-    
-
-    <div>
-        <img src="\assets\img\killua_header2.png" class="max-w-sm ml-10 lg:ml-20" alt="Killua">
-    </div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
 </template>
