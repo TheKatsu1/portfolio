5614a441871a38428c252c2728250e920f25cd02
Katsu <xjverudonx@gmail.com>
2023-05-09 18:37:14 -0300

    Updates

---
+++ Habilidades.afa9a6e0.js
@@ -0,0 +1,40 @@
+import{_ as s}from"./chunks/_plugin-vue_export-helper.1e5d64a6.js";import{c as a,d as e,a as r,b as d,o as l}from"./chunks/runtime-core.esm-bundler.a7da2ca4.js";const i={},o={class:"bg-klwhite pt-32",id:"Habilidades"},t=r("div",{class:"flex-1"},[r("h2",{class:"text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-kldarkblue to-klblue text-center"}," Habilidades")],-1),g=d('<div class="grid grid-cols-2 gap-4 p-8 lg:grid-cols-4"><div class="rounded-xl card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\html.png" alt=""></figure></div><div class="rounded-xl card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\css.png" alt=""></figure></div><div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\js.png" alt=""></figure></div><div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\vue.png" alt=""></figure></div><div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\bootstrap.png" alt=""></figure></div><div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\tailwind.png" alt=""></figure></div><div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\git.png" alt=""></figure></div><div class="rounded-lg card bg-base-100 drop-shadow-md group border-2 border-kldarkblue bg-white"><figure class="p-8"><img class="w-32 duration-300 group-hover:scale-110 m-auto" src="assets\\img\\php.png" alt=""></figure></div></div>',1);function c(n,u){return l(),a("div",o,[t,e(` <div class="flex flex-row mt-24 ">\r
+\r
+<div clas="">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+    <span class="text-center">html</span>\r
+</div>\r
+\r
+<div clas="border-1 border-red">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+<div clas="border-1 border-red">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+<div clas="border-1 border-red">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+</div>\r
+\r
+<div class="flex flex-row mt-14 ">\r
+\r
+<div clas="">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+<div clas="border-1 border-red">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+<div clas="border-1 border-red">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+<div clas="border-1 border-red">\r
+    <img src="\\assets\\img\\html.png" class="w-3/12 m-auto" alt="Killua">\r
+</div>\r
+\r
+</div> `),g])}const p=s(i,[["render",c]]);export{p as default};
